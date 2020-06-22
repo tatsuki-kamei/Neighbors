3f532582acc3eda6abc8bdcd80b74b2a2dd0a6fb
vagrant <kameitatsuki0624@gmail.com>
2020-06-22 02:26:21 +0000

    layout1 end

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,5 @@
 #     https: false
 #   )
 # end
+
+Refile.secret_key = '3dd3cbdf2f51206aad73062e0e6755c72379ca49578afd9b7e5839a42f1de7dd2c2de07b90f5381606747d3ecb33a3a90b514f3ebf61b0ce943f0f272f432c0d'
